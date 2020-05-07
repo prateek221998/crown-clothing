ca28644ea063da4e305c5dcd87f8223b840efefe
unknown <dwivedi.prateek22@gmail.com>
2020-05-08 03:05:25 +0530

    Updates

---
+++ precache-manifest.4d7174115524598184cb0582a2a56151.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c87a770836e0bd8bf11c3d265e40cc42",
+    "url": "/crown-clothing/index.html"
+  },
+  {
+    "revision": "1ff7f2e013058058f6b0",
+    "url": "/crown-clothing/static/css/main.b1b1f84c.chunk.css"
+  },
+  {
+    "revision": "21efe5aacccd7733ae17",
+    "url": "/crown-clothing/static/js/2.9b08d584.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/crown-clothing/static/js/2.9b08d584.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ff7f2e013058058f6b0",
+    "url": "/crown-clothing/static/js/main.7f48899c.chunk.js"
+  },
+  {
+    "revision": "884960c478b8c85316b7",
+    "url": "/crown-clothing/static/js/runtime-main.6a9c89e3.js"
+  }
+]);
